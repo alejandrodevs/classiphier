17c02bd660da14f9f7f13e50b84c3a591d90f7d7
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2015-07-17 17:51:09 -0500

    Adding examples

--- README.md
+++ README.md
@@ -22,6 +22,9 @@ require 'classiphier'
 
 classifier = Classiphier::Bayes.new
 classifier.train :positive, 'This is a great tweet. I like it!'
+classifier.train :positive, 'With great power comes great responsibility'
 classifier.train :negative, 'This tweet is not good. I hate it!'
+classifier.train :negative, 'Here are some bad words, I hate you'
+
 classifier.classify 'I like these tweets' # => :positive
 ```
--- test/classiphier/bayes_test.rb
+++ test/classiphier/bayes_test.rb
@@ -4,7 +4,9 @@ class ClassiphierBayesTest < Minitest::Test
   def setup
     @classifier = Classiphier::Bayes.new
     @classifier.train :positive, 'This is a great tweet. I like it!'
+    @classifier.train :positive, 'With great power comes great responsibility'
     @classifier.train :negative, 'This tweet is not good. I hate it!'
+    @classifier.train :negative, 'Here are some bad words, I hate you'
   end
 
   def test_classify
